4fd60c13e8b55951eeba210c1b8ca41b13e34b86
Tyler Southwick <tyler@northfuse.io>
2020-02-19 22:26:25 -0800

    update formatting for flake8

--- keyrings/lpass/__init__.py
+++ keyrings/lpass/__init__.py
@@ -1,24 +1,24 @@
 from keyring.backend import KeyringBackend
-from keyring import errors
 from keyring.util import properties
 from keyrings.lpass.lpass import load_notes, save_notes
 from keyrings.lpass.cli import lastpass_installed
 
+# from keyring import errors
+
 PRIORITY = 10
+
 
 class LastPassKeyring(KeyringBackend):
     @properties.ClassProperty
     @classmethod
     def priority(cls):
         if not lastpass_installed():
-            raise RuntimeError(
-                "Requires lastpass cli"
-            )
+            raise RuntimeError("Requires lastpass cli")
 
         return PRIORITY
 
     def get_password(self, service, username):
-        #print("get: " + service + " " + username)
+        # print("get: " + service + " " + username)
         notes = load_notes(service)
         if username in notes:
             return notes[username]
@@ -26,14 +26,14 @@ class LastPassKeyring(KeyringBackend):
             return None
 
     def set_password(self, service, username, password):
-        #print("set: " + service + " " + username + " " + password)
+        # print("set: " + service + " " + username + " " + password)
         notes = load_notes(service)
         notes[username] = password
         save_notes(service, notes)
-        #raise errors.PasswordSetError("reason")
+        # raise errors.PasswordSetError("reason")
 
     def delete_password(self, service, username):
-        #print("delete: " + service + " " + username)
+        # print("delete: " + service + " " + username)
         notes = load_notes(service)
         del notes[username]
         save_notes(service, notes)
--- keyrings/lpass/cli.py
+++ keyrings/lpass/cli.py
@@ -1,11 +1,14 @@
 import shutil
 import subprocess
 
+
 class AccountNotFound(Exception):
     pass
 
+
 def lastpass_installed():
     return bool(shutil.which("lpass"))
+
 
 def _convert_error(output):
     if "Could not find specified account" in output:
@@ -14,10 +17,13 @@ def _convert_error(output):
         print(output)
         raise Exception("unknown output: " + output)
 
+
 def lpass(*args, input=None):
     cli_args = ["lpass"] + list(args)
     try:
-        result = subprocess.run(cli_args, check=True, capture_output=True, input=input)
+        result = subprocess.run(
+            cli_args, check=True, capture_output=True, input=input
+        )  # noqa
         return result.stdout.decode("utf-8")
     except subprocess.CalledProcessError as exc:
         output = exc.stderr.decode("utf-8")
